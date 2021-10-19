a792d8a36f042521a680639235e5da486eeaa83e
Colin Torney <colin.j.torney@gmail.com>
2021-10-19 15:53:22 +0100

    reading and writing training data

---
+++ simulations/zonal_gnn.py
@@ -0,0 +1,205 @@
+import numpy as np
+from math import *
+import sys
+import random
+import time
+
+from tqdm import tqdm
+import tensorflow as tf
+import os
+
+def get_record(group_id,timestep,parameter_vector,pos,vel):
+    feature = { 'group_id': tf.train.Feature(int64_list=tf.train.Int64List(value=[group_id])),
+                'timestep': tf.train.Feature(int64_list=tf.train.Int64List(value=[timestep])),
+                'parameter_vector': tf.train.Feature(float_list=tf.train.FloatList(value=parameter_vector)),
+                'pos': tf.train.Feature(bytes_list=tf.train.BytesList(value=[pos.numpy()])),
+                'vel': tf.train.Feature(bytes_list=tf.train.BytesList(value=[vel.numpy()]))
+                }
+    return tf.train.Example(features=tf.train.Features(feature=feature))
+
+
+class zonal_model:
+    def __init__(self, N, timesteps, discard, repeat, L, dt, save_interval,data_directory='tmp_datasets', disable_progress=False):
+        self.N = N
+        self.timesteps = timesteps
+        self.discard = discard
+        self.B = repeat  # repeat for B batches
+        self.L = L
+        self.dt = dt
+        self.save_interval = save_interval
+        
+        self.micro_state = np.zeros((self.B, (self.timesteps - self.discard)//self.save_interval, N, 4))
+
+        self.sim_counter=0
+
+        if not os.path.exists(data_directory):
+            os.makedirs(data_directory)
+
+        self.data_directory = data_directory
+
+        # turn progress bar on or off
+        self.disable_progress = disable_progress
+
+    def initialise_state(self):
+
+        self.positions = tf.random.uniform((self.B,self.N,2),0.5*self.L, 0.5*self.L+20) #0,self.L)
+        #self.positions = tf.random.uniform((self.B,self.N,2),0, self.L) 
+        self.angles = tf.random.uniform((self.B,self.N,1), 0, 2*pi) #
+        
+
+
+    def run_sim(self, *params):
+
+        eta, Ra, Ro, Rr, vs, va, sigma = params
+        
+        record_file = self.data_directory + '/microstates-' + str(self.sim_counter) + '.tfrecords'
+        self.writer = tf.io.TFRecordWriter(record_file) 
+        
+        # tensorflow function to run an update step
+        @tf.function
+        def update_tf(X, A):
+            cos_A = tf.math.cos(A)
+            sin_A = tf.math.sin(A)
+
+
+            Xx = tf.expand_dims(X[...,0],-1)
+            dx = -Xx + tf.linalg.matrix_transpose(Xx)
+            dx = tf.where(dx>0.5*self.L, dx-self.L, dx)
+            dx = tf.where(dx<-0.5*self.L, dx+self.L, dx)
+
+            Xy = tf.expand_dims(X[...,1],-1)
+            dy = -Xy + tf.linalg.matrix_transpose(Xy)
+            dy = tf.where(dy>0.5*self.L, dy-self.L, dy)
+            dy = tf.where(dy<-0.5*self.L, dy+self.L, dy)
+
+
+            angle_to_neigh = tf.math.atan2(dy, dx)
+            cos_N = tf.math.cos(angle_to_neigh)
+            sin_N = tf.math.sin(angle_to_neigh)
+            rel_angle_to_neigh = angle_to_neigh - A
+            rel_angle_to_neigh = tf.math.atan2(tf.math.sin(rel_angle_to_neigh), tf.math.cos(rel_angle_to_neigh))
+            
+            dist = tf.math.sqrt(tf.square(dx)+tf.square(dy))
+    
+            # repulsion 
+            rep_x = tf.where(dist<=Rr, -dx, tf.zeros_like(dx))
+            rep_x = tf.where(rel_angle_to_neigh<0.5*va, rep_x, tf.zeros_like(rep_x))
+            rep_x = tf.where(rel_angle_to_neigh>-0.5*va, rep_x, tf.zeros_like(rep_x))
+            rep_x = tf.math.divide_no_nan(rep_x,tf.math.square(dist))
+            rep_x = tf.reduce_sum(rep_x,axis=2)
+
+            rep_y = tf.where(dist<=Rr, -dy, tf.zeros_like(dy))
+            rep_y = tf.where(rel_angle_to_neigh<0.5*va, rep_y, tf.zeros_like(rep_y))
+            rep_y = tf.where(rel_angle_to_neigh>-0.5*va, rep_y, tf.zeros_like(rep_y))
+            rep_y = tf.math.divide_no_nan(rep_y,tf.math.square(dist))
+            rep_y = tf.reduce_sum(rep_y,axis=2)
+
+            # alignment 
+            align_x = tf.where(dist<=Ro, cos_A, tf.zeros_like(cos_A))
+            align_x = tf.where(rel_angle_to_neigh<0.5*va, align_x, tf.zeros_like(align_x))
+            align_x = tf.where(rel_angle_to_neigh>-0.5*va, align_x, tf.zeros_like(align_x))
+            align_x = tf.reduce_sum(align_x,axis=1)
+            
+            align_y = tf.where(dist<=Ro, sin_A, tf.zeros_like(sin_A))
+            align_y = tf.where(rel_angle_to_neigh<0.5*va, align_y, tf.zeros_like(align_y))
+            align_y = tf.where(rel_angle_to_neigh>-0.5*va, align_y, tf.zeros_like(align_y))
+            align_y = tf.reduce_sum(align_y,axis=1)
+
+            al_norm = tf.math.sqrt(align_x**2+align_y**2)
+            align_x = tf.math.divide_no_nan(align_x,al_norm)
+            align_y = tf.math.divide_no_nan(align_y,al_norm)
+
+            # attractive interactions
+            attr_x = tf.where(dist<=Ra, dx, tf.zeros_like(dx))
+            attr_x = tf.where(rel_angle_to_neigh<0.5*va, attr_x, tf.zeros_like(attr_x))
+            attr_x = tf.where(rel_angle_to_neigh>-0.5*va, attr_x, tf.zeros_like(attr_x))
+            attr_x = tf.reduce_sum(attr_x,axis=2)
+
+            attr_y = tf.where(dist<=Ra, dy, tf.zeros_like(dy))
+            attr_y = tf.where(rel_angle_to_neigh<0.5*va, attr_y, tf.zeros_like(attr_y))
+            attr_y = tf.where(rel_angle_to_neigh>-0.5*va, attr_y, tf.zeros_like(attr_y))
+            attr_y = tf.reduce_sum(attr_y,axis=2)
+
+            at_norm = tf.math.sqrt(attr_x**2+attr_y**2)
+            attr_x = tf.math.divide_no_nan(attr_x,at_norm)
+            attr_y = tf.math.divide_no_nan(attr_y,at_norm)
+
+            # combine angles and convert to desired angle change
+            social_x = rep_x + align_x + attr_x
+            social_y = rep_y + align_y + attr_y
+
+            d_angle = tf.math.atan2(social_y,social_x)
+            d_angle = tf.expand_dims(d_angle,-1)
+
+            
+            d_angle = tf.math.atan2((1-eta)*tf.math.sin(d_angle) + eta*sin_A, (1-eta)*tf.math.cos(d_angle) + eta*cos_A)
+
+            d_angle = d_angle - A
+            d_angle = tf.where(d_angle>pi, d_angle-2*pi, d_angle)
+            d_angle = tf.where(d_angle<-pi, d_angle+2*pi, d_angle)
+
+
+            # add perception noise
+            noise = tf.random.normal(shape=(self.B,self.N,1),mean=0,stddev=sigma*(self.dt**0.5))
+            d_angle = d_angle + noise
+            
+            # restrict to maximum turning angle
+            #d_angle = tf.where(tf.math.abs(d_angle)>eta*self.dt, tf.math.sign(d_angle)*eta*self.dt, d_angle)
+            
+            # rotate headings
+            A = A + d_angle
+            
+            # update positions
+            velocity = self.dt*vs*tf.concat([tf.cos(A),tf.sin(A)],axis=-1)
+            X += velocity
+
+            # add periodic boundary conditions
+            A = tf.where(A<-pi,  A+2*pi, A)
+            A = tf.where(A>pi, A-2*pi, A)
+
+            X = tf.where(X>self.L, X-self.L, X)
+            X = tf.where(X<0, X+self.L, X)
+
+            X = tf.where(X>self.L, X-self.L, X)
+            X = tf.where(X<0, X+self.L, X)
+
+            return X, A
+            
+        self.initialise_state()
+
+        counter=0
+        for i in tqdm(range(self.timesteps),disable=self.disable_progress):
+            self.positions, self.angles = update_tf(self.positions,  self.angles)
+            if i>=self.discard:
+                if i%self.save_interval==0:
+                    # store in an array in case we want to visualise
+                    self.micro_state[:,counter,:,0:2] = self.positions.numpy()
+                    self.micro_state[:,counter,:,2:3] = np.cos(self.angles.numpy())
+                    self.micro_state[:,counter,:,3:4] = np.sin(self.angles.numpy())
+                        
+                    counter = counter + 1
+                    self.save_tf_record(counter, params)
+
+        self.writer.close()
+        self.sim_counter+=1
+        return 
+
+    def save_tf_record(self, counter, params):
+
+        A = self.angles
+
+        cos_A = tf.math.cos(A)
+        sin_A = tf.math.sin(A)
+
+        velocities = tf.concat([cos_A,sin_A],axis=-1)
+
+        for b in range(self.B):
+            pos =  tf.io.serialize_tensor(self.positions[b])
+            vel =  tf.io.serialize_tensor(velocities[b])
+
+            tf_record = get_record(b,counter,params,pos,vel)
+            self.writer.write(tf_record.SerializeToString())
+
+        
+        return 
+    
